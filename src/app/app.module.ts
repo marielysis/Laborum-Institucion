b142a56f134251f3f161b867827cfee8275242c2
Crusiris Nuñez <45858515+Crusiris@users.noreply.github.com>
2020-02-12 10:24:17 -0300

    Feature/components (#9)
    * Componentes del proyecto
    
    * Componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ManagementVacanciesComponent } from './components/management-vacancies/management-vacancies.component';
+import { LinkCompaniesComponent } from './components/link-companies/link-companies.component';
+import { PostulantsComponent } from './components/postulants/postulants.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ManagementVacanciesComponent,
+    LinkCompaniesComponent,
+    PostulantsComponent,
+   
+    NavBarComponent,
+   
+    FooterComponent,
+   
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
