e1c80923d3bdcf956e77e1ca89e4ed0addf9a675
Marielys Rodríguez <53873085+marielysis@users.noreply.github.com>
2020-02-12 15:50:41 -0300

    Te envio listo Header, siebar y footer (#11)
    * Feature/components (#9)
    
    * Componentes del proyecto
    
    * Componentes del proyecto
    
    * Siebar y header listos acoplados a componentes de react
    
    * footer listo
    
    * Probando rutas, home configurado
    
    Co-authored-by: Crusiris Nuñez <45858515+Crusiris@users.noreply.github.com>
    Co-authored-by: SusejR <57444604+SusejR@users.noreply.github.com>

---
+++ src/app/components/link-companies/link-companies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-link-companies',
+  templateUrl: './link-companies.component.html',
+  styleUrls: ['./link-companies.component.css']
+})
+export class LinkCompaniesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
