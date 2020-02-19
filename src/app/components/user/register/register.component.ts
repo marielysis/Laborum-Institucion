23ff322500396bb8a5ea3a95efabfb1d6460ed52
Mariajose <mariajosegomezcampo@gmail.com>
2020-02-19 12:12:59 -0500

    funcionalidad de login y register funcionando  mas algo de css

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 
 
 import { Component, OnInit ,NgZone} from '@angular/core';
 import { FormBuilder, Validators} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-=======
-import { Component, OnInit } from '@angular/core';
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
-<<<<<<< HEAD
 }) 
 export class RegisterComponent implements OnInit {
   errorMessage = '';
@@ -35,14 +30,4 @@ export class RegisterComponent implements OnInit {
        this.errorMessage = response.message;
      });
     }
-=======
-})
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-<<<<<<< HEAD
-=======
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, Query} from '@angular/fire/firestore';
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 
 @Injectable({
   providedIn: 'root'
@@ -12,34 +8,14 @@ export class AuthService {
 
   constructor(private afsAuth: AngularFireAuth) { }
 
-  // registar nuevo usuario
-<<<<<<< HEAD
 
 }
 
-  // metodo iniciar sesion con contraseña y email
-
-
-  
-  // cerrar sesion
 
 
 
-=======
-registerUser() {
-  
-}
+ 
 
-  // metodo iniciar sesion con contraseña y email
-loginEmailUser() {
-  
-}
-  
-  // cerrar sesion
-  logoutUser() {
-   
-  }
 
-}
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
+
 
